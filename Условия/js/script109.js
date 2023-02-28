1f034e5ecca003287fc694d1a50219e90243dbda
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-02-28 20:34:06 +0300

    task109

---
+++ Условия/js/script109.js
@@ -0,0 +1,12 @@
+let month = 12;
+if (month == 1 || month == 2 || month == 12){
+    console.log("зима")
+} else if (month == 3 || month == 4 || month == 5){
+    console.log("весна")
+} else if (month == 6 || month == 7 || month == 8){
+    console.log("лето")
+} else if (month == 9 || month == 10 || month == 11){
+    console.log("осень")
+} else {
+    console.log("wrong number")
+}
