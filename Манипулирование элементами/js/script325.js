0106567739d13120a702d757384d22a2193d0be3
Mavile Bekirova <mavilebekirova@MacBook-Air-Mavile.local>
2023-04-26 19:40:40 +0300

    task326

--- Манипулирование элементами/js/script325.js
+++ Манипулирование элементами/js/script325.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    function cloneInput() {
+function cloneInput() {
   var input = document.getElementById("input-field"); 
   var clonedInput = input.cloneNode(true); 
   input.parentNode.insertBefore(clonedInput, input.nextSibling); 
